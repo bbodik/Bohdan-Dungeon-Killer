601358d2eaff5cef629b407bf1558ec9a6615358
bohdan <bbodikk06@gmail.com>
2023-03-19 21:15:55 +0200

    added effects to Start

--- _JV_CD/src/Frame.java
+++ _JV_CD/src/Frame.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class Frame extends JFrame {
     Frame() {
@@ -18,21 +19,43 @@ public class Frame extends JFrame {
         JLabel background = new JLabel(new ImageIcon("images/BackgroundMenu.png"));
         JLabel start = new JLabel(new ImageIcon("images/ButtonStart.png"));
         JLabel exid = new JLabel(new ImageIcon("images/ButtonExid.png"));
+        JLabel effects = new JLabel(new ImageIcon("images/effects.gif"));
 
         background.setBounds(0, 0, 1366, 768);
         start.setBounds(50, 50, 382, 72);
-        exid.setBounds(50, 100, 280, 74);
+        exid.setBounds(50, 144, 280, 74);
+        effects.setBounds(0,0,390,80);
+        effects.setVisible(false);
 
         start.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
+                System.out.println("1");
+            }
+        });
+        start.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                effects.setLocation(45,45);
+                effects.setVisible(true);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                effects.setVisible(false);
+            }
+        });
+        exid.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
             }
         });
 
+        background.add(start);
+        background.add(exid);
+        background.add(effects);
         add(background);
-        add(start);
-        add(exid);
         repaint();
     }
 }
